03b7b1d586801e0f7d432a499420da51b7f54447
Ayaz Alam <ayaz.22scse1010228@galgotiasuniversity.edu.in>
2025-10-05 00:25:32 +0530

    Update FuctionSyntax.cpp

--- FuctionSyntax.cpp
+++ FuctionSyntax.cpp
@@ -1,3 +1,5 @@
+
+//find sum
 #include<iostream>
 using namespace std;
 int sum(int a,int b,int c,int d){
@@ -7,4 +9,48 @@ int sum(int a,int b,int c,int d){
 int main(){
     int s= sum (1,2,2,2);
     cout<<s<<endl;
+}
+
+
+
+
+//Product of two no code
+int product(int a, int b){
+    int product= a*b;
+return product;
+}
+int main(){
+    int p=product(3,4);
+cout<<p;}
+
+
+
+
+//ODD OR EVEN
+int oddEVEN(int a){
+   if(a%2==0){
+       cout<<"Even.\n";
+   }
+   else{
+   cout<<"odd.\n";}
+ return a;
+}
+
+int main(){
+    int p=oddEVEN(4);
+    
+}
+
+
+//Factorial using function
+int fact(int a){
+    int factorial =1;
+   for(int i=1;i<=a;i++){
+       factorial*=i;
+   }
+ return factorial;
+}
+
+int main(){
+   cout<< fact(5);
 }
