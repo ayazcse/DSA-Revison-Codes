bbb1293bfa1843193aadd2aba74b7ed2509ffcaf
Ayaz Alam <ayaz.22scse1010228@galgotiasuniversity.edu.in>
2025-10-05 00:47:16 +0530

    Update FuctionSyntax.cpp

--- FuctionSyntax.cpp
+++ FuctionSyntax.cpp
@@ -54,3 +54,22 @@ int fact(int a){
 int main(){
    cout<< fact(5);
 }
+
+
+
+
+//is Prime or Not
+bool isPrime(int n){
+    if(n==1){
+        return false;
+    }
+    for(int i=2;i<=n-1;i++){
+        if(n%i==0){
+            return false;
+        }
+    }
+    return true;
+}
+int main(){
+    cout<<isPrime(3);
+}
