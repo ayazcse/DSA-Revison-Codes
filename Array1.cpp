2b765a314312c404ac05a1ae8d78940c57f8b0f9
Ayaz Alam <ayaz.22scse1010228@galgotiasuniversity.edu.in>
2025-10-06 01:14:41 +0530

    Update Array1.cpp

--- Array1.cpp
+++ Array1.cpp
@@ -31,3 +31,42 @@ for(int i=0;i<n;i++){
 cout<<"Maximum :"<<max<<endl;
 
 }
+
+
+//Linear Search
+int LinearSerach(int *arr, int n , int key){
+  for(int i=0;i<n;i++){
+        if(arr[i]==key){
+                return i;
+        }
+  }
+  return -1;
+}
+int main(){
+int arr[]= {2,3,4,5,6,7};
+int n = sizeof(arr)/sizeof(int);
+cout<<LinearSerach(arr,n,6);
+}
+
+
+
+//Reverse Array 2 pointer without extra space
+void ReverseArray(int *arr, int n){
+        for(int i=0;i<n;i++){
+                cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+}
+int main(){
+        int arr[]= { 1, 2, 3, 4, 5 };
+        int n= sizeof(arr)/sizeof(int);
+        int start=0,end=n-1;
+        while(start<end){
+                int temp=arr[start];
+                arr[start]=arr[end];
+                arr[end]=temp;
+                start++;
+                end --;
+        }
+        ReverseArray(arr,n);
+}
