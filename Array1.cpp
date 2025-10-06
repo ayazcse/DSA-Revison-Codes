8dfe628fc25ab3e809ea1534128870385646d022
Ayaz Alam <ayaz.22scse1010228@galgotiasuniversity.edu.in>
2025-10-06 22:15:50 +0530

    Update Array1.cpp

--- Array1.cpp
+++ Array1.cpp
@@ -70,3 +70,30 @@ int main(){
         }
         ReverseArray(arr,n);
 }
+
+
+
+//Binary Search is only for sorted arrays TC= O(logn)
+ int BinarySearch(int *arr, int n, int key){
+     int start=0, end=n-1;
+     while(start<=end){
+         int mid=(start+end)/2;
+         if(mid==key){
+             return mid;
+         }
+         else if(mid>key){
+             end=mid-1;
+             
+         }
+         else if(mid<key){
+           start=mid+1;
+         }
+     }
+     return -1;
+ }
+ 
+ int main(){
+     int arr[]= { 1, 2, 3, 4, 5};
+     int n=sizeof(arr)/sizeof(int);
+     cout<<BinarySearch(arr,n,0)<<endl;
+ }
