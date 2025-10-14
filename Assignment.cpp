91447a7750ac040de41ab2681f1d5d868a39e195
Ayaz Alam <ayaz.22scse1010228@galgotiasuniversity.edu.in>
2025-10-14 23:47:09 +0530

    Update Assignment.cpp

--- Assignment.cpp
+++ Assignment.cpp
@@ -1,1 +1,100 @@
-.
+// {1,1,2,3,4} = true {2,3,3,2,3,3}=true {1,2,3}=false
+#include <iostream>
+#include <climits>
+using namespace std;
+
+bool isTwice( int *arr, int n){
+    for(int i=0;i<n;i++){
+        for(int j=i+1;i<n;j++){
+        if(arr[i]==arr[j]){
+            return true;
+        }
+            
+        }
+    }
+    return false;
+}
+int main(){
+    int arr[]= {0,1,2,3};
+    int n=4;
+   cout<< isTwice(arr,n);
+}
+
+
+
+
+//Sorted Rotated ....Target given
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int SearchRotated(int *arr, int n, int target){
+    int start=0, end=n-1;
+    
+    while(start<=end){
+        int mid= (start+end)/2;
+        if(arr[mid]==target){
+            return mid;
+        }
+    
+    
+    if(arr[start]<=arr[mid]){
+        if(target>=arr[start] && target<=arr[mid]){
+            end =mid-1;
+        }
+        else {
+            start=mid+1;
+        }
+    }
+    else{
+        if(arr[end]>=target && arr[mid]<=target){
+            start= mid+1;
+        }
+        else{
+            end= mid-1;
+        }
+    }
+        
+    }
+    return -1;
+}
+int main(){
+    int arr[]= {6,7,8,9,0,1,2,3,4,5};
+    int n = sizeof(arr)/sizeof(int);
+    int target = 6;
+
+    int index = SearchRotated(arr,n,target);
+    if(index != -1)
+        cout << "Target found at index: " << index;
+    else
+        cout << "Target not found";
+}
+
+
+
+
+//Maximum Product Subarray – Super Easy Version
+#include<iostream>
+#include<climits>
+using namespace std;
+int maaxProduct( int *arr, int n){
+    int maxSoFar=arr[0];
+    int minSoFar=arr[0];
+    int result=arr[0];
+    
+    for(int i=1;i<n;i++){
+        if(arr[i]<0) swap(maxSoFar, minSoFar);
+        
+        maxSoFar= max(arr[i], arr[i]*maxSoFar);
+        minSoFar= min(arr[i],arr[i]*minSoFar);
+        result=max(result,maxSoFar);
+    }
+    return result;
+}
+
+int main(){
+    int arr[] = {1,2,-4,-2};
+    int n=4;
+    cout<<"Maximum Subarray : "<<maaxProduct(arr,n)<<endl;
+}
+
