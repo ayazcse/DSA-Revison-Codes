7bd009d9c998dfe0ebc3fd726b85be16a8d89fca
Ayaz Alam <ayaz.22scse1010228@galgotiasuniversity.edu.in>
2025-10-17 20:10:11 +0530

    Update FuctionSyntax.cpp

--- FuctionSyntax.cpp
+++ FuctionSyntax.cpp
@@ -1,102 +1,170 @@
-import os
-import streamlit as st
-
-from langchain_huggingface import HuggingFaceEmbeddings
-from langchain.chains import RetrievalQA
-
-from langchain_community.vectorstores import FAISS
-from langchain_core.prompts import PromptTemplate
-from langchain_huggingface import HuggingFaceEndpoint
-from langchain_groq import ChatGroq
-
-
-## Uncomment the following files if you're not using pipenv as your virtual environment manager
-#from dotenv import load_dotenv, find_dotenv
-#load_dotenv(find_dotenv())
+//find sum
+#include<iostream>
+using namespace std;
+int sum(int a,int b,int c,int d){
+    int sum= a+b+c+d;
+    return sum;
+}
+int main(){
+    int s= sum (1,2,2,2);
+    cout<<s<<endl;
+}
 
 
-DB_FAISS_PATH="vectorstore/db_faiss"
-@st.cache_resource
-def get_vectorstore():
-    embedding_model=HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
-    db=FAISS.load_local(DB_FAISS_PATH, embedding_model, allow_dangerous_deserialization=True)
-    return db
 
 
-def set_custom_prompt(custom_prompt_template):
-    prompt=PromptTemplate(template=custom_prompt_template, input_variables=["context", "question"])
-    return prompt
+//Product of two no code
+int product(int a, int b){
+    int product= a*b;
+return product;
+}
+int main(){
+    int p=product(3,4);
+cout<<p;}
 
 
-def load_llm(huggingface_repo_id, HF_TOKEN):
-    llm=HuggingFaceEndpoint(
-        repo_id=huggingface_repo_id,
-        temperature=0.5,
-        model_kwargs={"token":HF_TOKEN,
-                      "max_length":"512"}
-    )
-    return llm
 
 
-def main():
-    st.title("Ask Chatbot!")
+//ODD OR EVEN
+int oddEVEN(int a){
+   if(a%2==0){
+       cout<<"Even.\n";
+   }
+   else{
+   cout<<"odd.\n";}
+ return a;
+}
 
-    if 'messages' not in st.session_state:
-        st.session_state.messages = []
+int main(){
+    int p=oddEVEN(4);
 
-    for message in st.session_state.messages:
-        st.chat_message(message['role']).markdown(message['content'])
+}
 
-    prompt=st.chat_input("Pass your prompt here")
 
-    if prompt:
-        st.chat_message('user').markdown(prompt)
-        st.session_state.messages.append({'role':'user', 'content': prompt})
+//Factorial using function
+int fact(int a){
+    int factorial =1;
+   for(int i=1;i<=a;i++){
+       factorial*=i;
+   }
+ return factorial;
+}
 
-        CUSTOM_PROMPT_TEMPLATE = """
-                Use the pieces of information provided in the context to answer user's question.
-                If you dont know the answer, just say that you dont know, dont try to make up an answer. 
-                Dont provide anything out of the given context
+int main(){
+   cout<< fact(5);
+}
 
-                Context: {context}
-                Question: {question}
 
-                Start the answer directly. No small talk please.
-                """
-        
-        #HUGGINGFACE_REPO_ID="mistralai/Mistral-7B-Instruct-v0.3" # PAID
-        #HF_TOKEN=os.environ.get("HF_TOKEN")  
 
-        #TODO: Create a Groq API key and add it to .env file
-        
-        try: 
-            vectorstore=get_vectorstore()
-            if vectorstore is None:
-                st.error("Failed to load the vector store")
 
-            qa_chain = RetrievalQA.from_chain_type(
-                llm=ChatGroq(
-                    model_name="meta-llama/llama-4-maverick-17b-128e-instruct",  # free, fast Groq-hosted model
-                    temperature=0.0,
-                    groq_api_key=os.environ["GROQ_API_KEY"],
-                ),
-                chain_type="stuff",
-                retriever=vectorstore.as_retriever(search_kwargs={'k':3}),
-                return_source_documents=True,
-                chain_type_kwargs={'prompt': set_custom_prompt(CUSTOM_PROMPT_TEMPLATE)}
-            )
+//is Prime or Not
+bool isPrime(int n){
+    if(n==1){
+        return false;
+    }
+    for(int i=2;i<=n-1;i++){
+        if(n%i==0){
+            return false;
+        }
+    }
+    return true;
+}
+int main(){
+    cout<<isPrime(3);
+}
 
-            response=qa_chain.invoke({'query':prompt})
 
-            result=response["result"]
-            source_documents=response["source_documents"]
-            result_to_show=result+"\nSource Docs:\n"+str(source_documents)
-            #response="Hi, I am MediBot!"
-            st.chat_message('assistant').markdown(result_to_show)
-            st.session_state.messages.append({'role':'assistant', 'content': result_to_show})
 
-        except Exception as e:
-            st.error(f"Error: {str(e)}")
+//Binomial Coefficient
+//Binomial Coefficient   BC= n!/r!*(n-r)!
+int fact(int n){
+    int f=1;
+for(int i=1;i<=n;i++){
+   f*=i;
+}
+return f;
+}
 
-if __name__ == "__main__":
-    main()
+int bc(int n,int r){
+    int val1= fact(n);
+int val2= fact(r);
+int val3= fact(n-r);
+int result= val1/(val2*val3);
+return result;
+}
+
+int main(){
+cout<<bc(5,5);
+}
+
+
+//Prime no upto n
+bool isPrime(int n){
+    if(n==1){
+        return false;
+    }
+    for(int i=2;i<=n-1;i++){
+    if(n%i==0){
+     return false;   
+    }
+    }
+    return true;
+}
+void allPrime(int n){
+    for(int i=2;i<=n;i++){
+        if(isPrime(i)){
+            cout<<i<<endl;
+        }
+    }
+}
+int main(){
+    allPrime(20);
+    return 0;
+}
+
+
+
+
+//Sum of Digits
+int sumOfNum(int n){
+    int sum=0;
+    while(n>0){
+       int reverse= n%10;
+       sum+= reverse;
+       n=n/10;
+    }
+    return sum;
+}
+int main(){
+    int number;
+    cin>>number;
+   cout<< sumOfNum(number);
+}
+
+
+
+//example
+int paramters(int a, int b){
+    int sum= (a*a)+(b*b)+(2*a*b);
+    return sum;
+}
+int main(){
+    int a,b;
+    cin>>a;
+    cin>>b;
+    cout<<paramters(a,b);
+}
+
+
+
+//example 2
+char interchange(char ch){
+    if(ch=='Z') return 'A';
+    if(ch=='z') return 'a';
+    return ch+1;
+}
+int main(){
+   char ch;
+   cin>>ch;
+   cout<<interchange(ch);
+}
