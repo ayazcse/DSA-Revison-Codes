f104771e5651d7bc6fc64196b75133c36643c8bf
Ayaz Alam <ayaz.22scse1010228@galgotiasuniversity.edu.in>
2025-10-23 13:05:43 +0530

    Update TwoDarrays.cpp

--- TwoDarrays.cpp
+++ TwoDarrays.cpp
@@ -23,3 +23,53 @@ for(int i=0;i<n;i++){
     cout<<endl;
 }
     }
+
+
+//SPIRAL MATRIX
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    cout << "Rows aur Columns batao babu: ";
+    cin >> n >> m;
+
+    int arr[n][m];
+    cout << "Matrix ke elements daalo:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    int top = 0;
+    int bottom = n - 1;
+    int left = 0;
+    int right = m - 1;
+
+    cout << "\nSpiral Order: ";
+
+    while (top <= bottom && left <= right) {
+        for (int i = left; i <= right; i++)
+            cout << arr[top][i] << " ";
+        top++;
+
+        for (int i = top; i <= bottom; i++)
+            cout << arr[i][right] << " ";
+        right--;
+
+        if (top <= bottom) {
+            for (int i = right; i >= left; i--)
+                cout << arr[bottom][i] << " ";
+            bottom--;
+        }
+
+        if (left <= right) {
+            for (int i = bottom; i >= top; i--)
+                cout << arr[i][left] << " ";
+            left++;
+        }
+    }
+
+    return 0;
+}
