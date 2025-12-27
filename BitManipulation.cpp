89bf3bbf75025a5f294317e4307d79bf07692a0c
Ayaz Alam <ayaz.22scse1010228@galgotiasuniversity.edu.in>
2025-12-27 09:16:02 +0530

    Update BitManipulation.cpp

--- BitManipulation.cpp
+++ BitManipulation.cpp
@@ -19,3 +19,28 @@ int main()
     
     return 0;
 }
+
+
+//GET iTH BIT
+//mask= 1 Left Shit 1   -if num & bitmask == 0  ans=0 , otherwise 1
+
+#include <iostream>
+using namespace std;
+void GethBit(int n, int i){
+    int bitmask= 1<<i;
+    if((n&bitmask)==0){
+            cout<<"0";
+    }
+    else{
+            cout<<"1";
+    }
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    GethBit(n,3);
+    
+    return 0;
+}
